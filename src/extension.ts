4100ce29fa12323ccec61fec600b95cc844dd509
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2024-09-12 16:09:37 -0700

    Attempt to add hot reload

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,9 @@
+import {
+  enableHotReload,
+  hotRequireExportedFn,
+  registerUpdateReconciler,
+} from "@hediet/node-reload";
+import { Disposable } from "@hediet/std/disposable";
 import * as vscode from "vscode";
 import { ViewKey } from "./views";
 import { registerView } from "./registerView";
@@ -11,88 +17,101 @@ import {
 } from "./viewApi";
 import fs from "node:fs/promises";
 
-export const activate = async (ctx: vscode.ExtensionContext) => {
-  const connectedViews: Partial<Record<ViewKey, vscode.WebviewView>> = {};
+enableHotReload({ entryModule: module });
+registerUpdateReconciler(module);
 
-  const triggerEvent = <E extends keyof ViewEvents>(
-    key: E,
-    ...params: Parameters<ViewEvents[E]>
-  ) => {
-    Object.values(connectedViews).forEach((view) => {
-      view.webview.postMessage({
-        type: "event",
-        key,
-        value: params,
-      } as ViewApiEvent<E>);
-    });
-  };
+export class Extension {
+  public readonly dispose = Disposable.fn();
 
-  const api: ViewApi = {
-    getFileContents: async () => {
-      const uris = await vscode.window.showOpenDialog({
-        canSelectFiles: true,
-        canSelectFolders: false,
-        canSelectMany: false,
-        openLabel: "Select file",
-        title: "Select file to read",
+  constructor(ctx: vscode.ExtensionContext) {
+    const connectedViews: Partial<Record<ViewKey, vscode.WebviewView>> = {};
+
+    const triggerEvent = <E extends keyof ViewEvents>(
+      key: E,
+      ...params: Parameters<ViewEvents[E]>
+    ) => {
+      Object.values(connectedViews).forEach((view) => {
+        view.webview.postMessage({
+          type: "event",
+          key,
+          value: params,
+        } as ViewApiEvent<E>);
       });
-
-      if (!uris?.length) {
-        return "";
-      }
-
-      const contents = await fs.readFile(uris[0].fsPath, "utf-8");
-      return contents;
-    },
-    showExampleViewB: () => {
-      connectedViews?.exampleViewB?.show?.(true);
-      vscode.commands.executeCommand(`exampleViewB.focus`);
-    },
-    sendMessageToExampleB: (msg: string) => {
-      triggerEvent("exampleBMessage", msg);
-    },
-  };
-
-  const isViewApiRequest = <K extends keyof ViewApi>(
-    msg: unknown
-  ): msg is ViewApiRequest<K> =>
-    msg != null &&
-    typeof msg === "object" &&
-    "type" in msg &&
-    msg.type === "request";
-
-  const registerAndConnectView = async <V extends ViewKey>(key: V) => {
-    const view = await registerView(ctx, key);
-    connectedViews[key] = view;
-    const onMessage = async (msg: Record<string, unknown>) => {
-      if (!isViewApiRequest(msg)) {
-        return;
-      }
-      try {
-        // @ts-expect-error
-        const val = await Promise.resolve(api[msg.key](...msg.params));
-        const res: ViewApiResponse = {
-          type: "response",
-          id: msg.id,
-          value: val,
-        };
-        view.webview.postMessage(res);
-      } catch (e: unknown) {
-        const err: ViewApiError = {
-          type: "error",
-          id: msg.id,
-          value:
-            e instanceof Error ? e.message : "An unexpected error occurred",
-        };
-        view.webview.postMessage(err);
-      }
     };
 
-    view.webview.onDidReceiveMessage(onMessage);
-  };
+    const api: ViewApi = {
+      getFileContents: async () => {
+        const uris = await vscode.window.showOpenDialog({
+          canSelectFiles: true,
+          canSelectFolders: false,
+          canSelectMany: false,
+          openLabel: "Select file",
+          title: "Select file to read",
+        });
 
-  registerAndConnectView("exampleViewA");
-  registerAndConnectView("exampleViewB");
+        if (!uris?.length) {
+          return "";
+        }
+
+        const contents = await fs.readFile(uris[0].fsPath, "utf-8");
+        return contents;
+      },
+      showExampleViewB: () => {
+        connectedViews?.exampleViewB?.show?.(true);
+        vscode.commands.executeCommand(`exampleViewB.focus`);
+      },
+      sendMessageToExampleB: (msg: string) => {
+        triggerEvent("exampleBMessage", msg);
+      },
+    };
+
+    const isViewApiRequest = <K extends keyof ViewApi>(
+      msg: unknown
+    ): msg is ViewApiRequest<K> =>
+      msg != null &&
+      typeof msg === "object" &&
+      "type" in msg &&
+      msg.type === "request";
+
+    const registerAndConnectView = async <V extends ViewKey>(key: V) => {
+      const view = await registerView(ctx, key);
+      connectedViews[key] = view;
+      const onMessage = async (msg: Record<string, unknown>) => {
+        if (!isViewApiRequest(msg)) {
+          return;
+        }
+        try {
+          // @ts-expect-error
+          const val = await Promise.resolve(api[msg.key](...msg.params));
+          const res: ViewApiResponse = {
+            type: "response",
+            id: msg.id,
+            value: val,
+          };
+          view.webview.postMessage(res);
+        } catch (e: unknown) {
+          const err: ViewApiError = {
+            type: "error",
+            id: msg.id,
+            value:
+              e instanceof Error ? e.message : "An unexpected error occurred",
+          };
+          view.webview.postMessage(err);
+        }
+      };
+
+      view.webview.onDidReceiveMessage(onMessage);
+    };
+
+    registerAndConnectView("exampleViewA");
+    registerAndConnectView("exampleViewB");
+  }
+}
+
+export const activate = async (ctx: vscode.ExtensionContext) => {
+  ctx.subscriptions.push(
+    hotRequireExportedFn(module, Extension, (Extension) => new Extension(ctx))
+  );
 };
 
 export const deactivate = () => {
